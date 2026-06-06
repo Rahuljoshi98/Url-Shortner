b6cf472e69dd7bebddd2f6ec2ffc597fb472cc62
rahuljoshi10 <rahuljoshi@kiikan.com>
2026-06-06 23:07:33 +0530

    refactor: replace strict CORS origin whitelist with wildcard access for development

--- src/app.js
+++ src/app.js
@@ -6,26 +6,9 @@ dotenv.config();
 
 const app = express();
 
-const defaultAllowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "http://127.0.0.1:3000",
-  "http://127.0.0.1:3001",
-];
-
-const allowedOrigins = process.env.CORS_ORIGINS
-  ? process.env.CORS_ORIGINS.split(",").map((origin) => origin.trim())
-  : defaultAllowedOrigins;
-
+// CORS – allow all origins in development
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Vary", "Origin");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-  }
-
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,POST,PUT,PATCH,DELETE,OPTIONS",
